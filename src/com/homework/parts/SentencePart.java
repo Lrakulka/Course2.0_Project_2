e706b361039f2fa40bd2cd2bd16afe3e52a32bba
Lrakulka <olecsandr32@gmail.com>
2016-05-04 11:54:02 +0300

    Sentence part interface.

---
+++ src/com/homework/parts/SentencePart.java
@@ -0,0 +1,16 @@
+package com.homework.parts;
+
+public interface SentencePart {
+    /**
+     * Return part of text.
+     * @return part of text
+     */
+    String getPart();
+    
+    /**
+     * Set part of text.
+     * @param part
+     */
+    void setPart(String part);
+    
+}
